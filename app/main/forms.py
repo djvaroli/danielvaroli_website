acb0a35dc802e4b0a86ee0119eaa1500d3deb5b9
Daniel Varoli <daniel.varoli@gmail.com>
2020-01-29 19:51:08 -0500

    added mobile view

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, TextAreaField,SubmitField
 from wtforms.validators import ValidationError, Email, DataRequired,DataRequired
 
 class ContactForm(FlaskForm):
-    name = StringField(render_kw={'placeholder':'Your name goes here'}, validators=[DataRequired()])
-    email = StringField('Email', render_kw={'placeholder':"I'm going to need your email as well"}, validators=[DataRequired(),Email()])
-    message = TextAreaField('Message', render_kw={'placeholder':'Your message goes here'}, validators=[DataRequired()])
+    name = StringField(render_kw={'placeholder':'My Name is'}, validators=[DataRequired()])
+    email = StringField('Email', render_kw={'placeholder':"My Email is"}, validators=[DataRequired(),Email()])
+    message = TextAreaField('Message', render_kw={'placeholder':'Dear Mr. Varoli,'}, validators=[DataRequired()])
     recaptcha = RecaptchaField("Just checking you are not AI")
     submit = SubmitField('Send')
