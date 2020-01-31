54e5cf12cf53e371e19f112d8e4abca377fc893b
Daniel Varoli <daniel.varoli@gmail.com>
2020-01-31 18:33:33 -0500

    Fixing config file error

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cc968ff9f1a1f59feb8bd83ad983ae3d3d565b74631cd7d9d2c7712d30258f86cc3c63fb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com' or os.environ.get('MAIL_SERVER')
+    MAIL_PORT = 587 or int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = 1 or os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = 'varolidaniel0' or os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = 'Uranium 238' or os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['varolidaniel0@gmail.com','daniel@danielvaroli.com']
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    RECAPTCHA_PUBLIC_KEY = '6Le_mNMUAAAAAEddyecx8fYaDdcsPY8rSuYHGMiy'
+    RECAPTCHA_PRIVATE_KEY = '6Le_mNMUAAAAAKeuPH-9DQnO0Gk6SUSgiP8o9rtH'
+    LANGUAGES = ['en']
+
+# '5016f9caa81821e1d64f'
